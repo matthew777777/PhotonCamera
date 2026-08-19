30ed58b2e3213da9c41b9d1ea1b727fb06b76dbe
matthew777777 <new.matthew7777@gmail.com>
2026-08-19 13:53:08 +0300

    Revert "Add real-time histogram and update project dependencies and Added AE Metering Mode for MTK Devices"
    This reverts commit e3c374dcf96323a1508fca7c32e98d02025e37ad.

--- app/src/main/java/com/particlesdevs/photoncamera/capture/CaptureController.java
+++ app/src/main/java/com/particlesdevs/photoncamera/capture/CaptureController.java
@@ -2433,12 +2433,6 @@ public class CaptureController implements MediaRecorder.OnInfoListener {
         }
     }
 
-    public void applyAeMetering() {
-        if (mPreviewRequestBuilder == null) return;
-        VendorTagUtils.builderSessionApply(mPreviewRequestBuilder, false, useMaximumResolutionKey);
-        rebuildPreviewBuilder();
-    }
-
     public void resetPreviewAEMode() {
         setAEMode(mPreviewRequestBuilder, PreferenceKeys.getAeMode());
     }
